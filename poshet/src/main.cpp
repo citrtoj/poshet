fba22282d16b71878d7f9f1ca937761337f85df9
Sabina <citrtoj@gmail.com>
2023-12-05 20:20:46 +0200

    update CMakeLists

--- poshet/src/main.cpp
+++ poshet/src/main.cpp
@@ -5,8 +5,6 @@
 #include "Pop3Connection.hpp"
 #include "LoginController.hpp"
 
-#include <SFML/Graphics.hpp>
-
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(Config::getInstance().RES_WIDTH, Config::getInstance().RES_HEIGHT), Config::getInstance().APP_NAME);
